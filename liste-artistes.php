551f578079602ebe4f84a75dc820f5f20253e968
leadals <lea.dalstein@outlook.fr>
2022-11-16 16:31:08 +0100

    Liste artistes

--- liste-artistes.php
+++ liste-artistes.php
@@ -40,6 +40,16 @@
 </head>
 
 <body>
+	<?php 
+		try{
+		$user="ldalstein_festivalmusique";
+		$password="Festival22!";
+		$dbh = new PDO('pgsql:host=postgresql-ldalstein.alwaysdata.net dbname=ldalstein_festivalmusique', $user, $password);
+		}
+		catch(PDOException $e){
+		die("erreur de connexion:".$e->getMessage());
+		}
+    ?>
 	<!-- En-tête avec barre de navigation -->
 	<header class="container">
 		<div class="d-flex flex-wrap justify-content-center px-4 py-3 mb-4 border-bottom">
@@ -75,63 +85,28 @@
 		<p class="lead">Cette page affiche la liste des artistes qui se produisent pendant le festival 2022 (ordre alphabétique).</p>
 
 		<div class="row my-5">
-			<div class="col-lg-3 col-md-4 col-sm-6 my-2">
-				<a href="fiche-artiste.php?id=15" class="lien-card">
-					<div class="card">
-						<img src="https://www.vercorsmusicfestival.com/media/cache/program_artist_large/uploads/artist_image/large/4afc24a470798044c26e489b85484df6009aa88d.jpeg" class="card-img-top" alt="Illustration artiste">
-						<div class="card-body">
-							<h5 class="card-title"><span class='donnee-bdd gras'>Adèle & Robin</span></h5>
-							<p class="card-text">
-								Jour du concert : <span class='donnee-bdd'>03/07/2022</span>
-							</p>
-						</div>
-					</div>
-				</a>
-			</div>
-
-			<div class="col-lg-3 col-md-4 col-sm-6 my-2">
-				<a href="fiche-artiste.php?id=7" class="lien-card">
-					<div class="card">
-						<img src="https://www.vercorsmusicfestival.com/media/cache/program_artist_large/uploads/artist_image/large/7c50b32e3b1e42208fdc6df1ad26106c7cbd88d0.jpeg" class="card-img-top" alt="Illustration artiste">
-						<div class="card-body">
-							<h5 class="card-title"><span class='donnee-bdd gras'>ARABELLA</span></h5>
-							<p class="card-text">
-								Jour du concert : <span class='donnee-bdd'>02/07/2022</span>
-							</p>
-						</div>
-					</div>
-				</a>
-			</div>
-
-			<div class="col-lg-3 col-md-4 col-sm-6 my-2">
-				<a href="fiche-artiste.php?id=23" class="lien-card">
-					<div class="card">
-						<img src="https://www.vercorsmusicfestival.com/media/cache/program_artist_large/uploads/artist_image/large/5ef8af816bc5ae6bbef10cc5c16edb03de6fa7e4.jpeg" class="card-img-top" alt="Illustration artiste">
-						<div class="card-body">
-							<h5 class="card-title"><span class='donnee-bdd gras'>Bark</span></h5>
-							<p class="card-text">
-								Jour du concert : <span class='donnee-bdd'>03/07/2022</span>
-							</p>
-						</div>
-					</div>
-				</a>
-			</div>
-
-			<div class="col-lg-3 col-md-4 col-sm-6 my-2">
-				<a href="fiche-artiste.php?id=4" class="lien-card">
-					<div class="card">
-						<img src="https://www.vercorsmusicfestival.com/media/cache/program_artist_large/uploads/artist_image/large/da8f87ecab95b2bc9a5aa536082386e5cbbab405.jpeg" class="card-img-top" alt="Illustration artiste">
-						<div class="card-body">
-							<h5 class="card-title"><span class='donnee-bdd gras'>Ben Mazué</span></h5>
-							<p class="card-text">
-								Jour du concert : <span class='donnee-bdd'>01/07/2022</span>
-							</p>
-						</div>
-					</div>
-				</a>
-			</div>
-
-			<p>...</p>
+			<?php
+                $req="select Artiste.id_artiste as artiste, nom_artiste, lien_illustration, date_concert from Artiste, Concert
+					where Artiste.id_artiste=Concert.id_artiste
+					order by nom_artiste asc";
+                $sth=$dbh->query($req);
+                $result=$sth->fetchAll(); 
+                foreach ($result as $row){
+                  ?> <div class="col-lg-3 col-md-4 col-sm-6 my-2">
+				  <a href="fiche-artiste.php?id=<?php echo $row['artiste'] ?>" class="lien-card">
+					  <div class="card">
+						  <img src="<?php echo $row['lien_illustration'] ?>" class="card-img-top" alt="Illustration artiste">
+						  <div class="card-body">
+							  <h5 class="card-title"><span class='donnee-bdd gras'> <?php echo $row['nom_artiste'] ?></span></h5>
+							  <p class="card-text">
+								  Jour du concert : <span class='donnee-bdd'> <?php echo $row['date_concert'] ?> </span>
+							  </p>
+						  </div>
+					  </div>
+				  </a>
+			  </div><?php ;
+                }
+            ?>
 
 		</div>
 	</main>
